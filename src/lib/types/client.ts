321b348043ffe01c649bdaf22f55ba744dbed7b2
立津 尚也 <tatetsu_naoya@cyberagent.co.jp>
2023-03-10 02:30:28 +0900

    getClientだけ

---
+++ src/lib/types/client.ts
@@ -0,0 +1,7 @@
+export type ClientType = {
+  id: number
+  name: string
+  age: number
+  disese: string
+  family_living_together: string | null
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Client from "./client/client"
+
+function Index(): JSX.Element {
+  return (
+    <>
+      <Client></Client>
+    </>
+  )
+}
+
+export default Index
